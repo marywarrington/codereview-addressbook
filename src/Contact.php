b5d2880b92cd1514012fdb0082fc95fc10036cd3
Mary <marywarrington@gmail.com>
2016-02-12 08:55:50 -0800

    Add getter and setter functions

--- src/Contact.php
+++ src/Contact.php
@@ -11,5 +11,52 @@ class Contact
         $this->phone = $phone;
         $this->address = $address;
     }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setName()
+    {
+        $this->name = (string) $name;
+    }
+
+    function getPhone()
+    {
+        return $this->phone;
+    }
+
+    function setPhone()
+    {
+        $this->phone = (string) $phone;
+    }
+
+    function getAddress()
+    {
+        return $this->address;
+    }
+
+    function setAddress()
+    {
+        $this->address = (string) $address;
+    }
+
+    function save()
+    {
+        array_push($_SESSION['list_of_contacts'], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_contacts'];
+    }
+
+    static function deleteAll()
+    {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
+
 }
 ?>
